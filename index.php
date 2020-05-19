db94b5a378a9014ce8a5c53fef47f8123acbda86
Lauwy de Hoop <55271928+DJLauwy@users.noreply.github.com>
2020-05-19 15:01:16 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!-- ©Lau de Hoop & Teun van der Ploeg | v0.1 -->
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<!-- <meta name="copyright" content="Copyrighted by &copy;Copyrighthouse http://www.copyrighthouse.nl/"> -->
+		<link rel="icon" href="#" type="image/icon type">
+		<meta name="author" content="Lau de Hoop & Teun van der Ploeg">
+		<meta name="description" content="#">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Project Covid-19 [work title]</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+
+	</head>
+	<body>
+		<nav>
+			<h1>Project Covid-19 [work title]</h1>
+			<ul>
+				<li>Home</li>
+				<li>Explore</li>
+				<li>Red Cross</li>
+				<li>Contact</li>
+			</ul>
+			<div>
+				<p><a href="#">Profile</a></p>
+				<img src="img/#">
+			</div>
+		</nav>
+		<main>
+			<article></article>
+			<aside></aside>
+		</main>
+		<footer></footer>
+	</body>
+</html>
